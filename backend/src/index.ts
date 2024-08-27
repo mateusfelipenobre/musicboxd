801403e2637f7c9333d53b92bc9622fb2f52b8c2
mateusfelipenobre <mateusfelipenobre@gmail.com>
2024-08-27 00:03:11 -0300

    construindo back
    construindo back

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,26 +3,92 @@ import express from 'express';
 const app = express();
 const port = 3000;
 
-app.get('*', (req, res) => {
-    res.send(`
-        <html>
-            <head>
-                <title>MusicBoxd</title>
-            </head>
-            <body>
-                <h1>MusicBoxd</h1>
-                <p>Rede social onde o usuário pode pesquisar um álbum de música,
-                 escrever uma resenha, editá-la e apagá-la caso queira.</p>
-                <h2>Autores</h2>
-                <ul>
-                    <li>Mateus Felipe</li>
-                    <li>Thais Alves</li>
-                   
-                </ul>
-            </body>
-        </html>
-    `);
-});
+ app.use(express.json())
+
+ interface User {
+    id: number;
+    name: string;
+    username: string;
+    email: string;
+    password: string;
+    role: string;
+ }
+
+ const users: User [] = []
+
+ app.post('/users', (req, res) => {
+    const {name, username, email, password, role} =req.body 
+
+    if(!name || !username || !email || !password || !role ){
+      return res.status(400).json({
+         error: {
+            status:400,
+            name: 'Erro de validação',
+            message:'You missed'
+         }
+      })
+    }
+
+    const newUser: User = {
+      id: users.length +1,
+      name,
+      username,
+      email,
+      password,
+      role
+    }
+
+    users.push(newUser)
+    res.status(200).json({
+      data: newUser
+    })
+ }) 
+
+ app.get('/users', (req, res) => {
+   res.json({
+      data: users
+   })
+ })
+ app.get('/users/:id', (req, res) => {
+   const { id } = req.params
+   const user = users.find(u => u.id === parseInt(id))
+
+   if(!user) {
+      return res.status(404).json({
+         error: {
+            status: 404,
+            name: 'NotFound',
+            message: 'User not found'
+         }
+      })
+   }
+
+   res.json({
+      data:user
+   })
+
+ })
+
+app.delete('/users/:id', (req, res) => {
+   const { id } = req.params
+   const userIndex = users.findIndex(u => u.id === parseInt(id))
+
+   if(userIndex === -1) {
+      return res.status(404).json({
+         error: {
+            status: 404,
+            name: 'NotFound',
+            message: 'User not found'
+         }
+      })
+   }
+
+   const deletedUser = users.splice(userIndex, 1) [0]
+   res.status(200).json({
+      data: deletedUser
+   })
+
+})
 
 app.listen(port, () => {
     console.log(`Servidor rodando em http://localhost:${port}`);
